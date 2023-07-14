39ccd6b74146d2866874e9c977d2634e9f6ebc33
Alexis Grant <alexisg@gmail.com>
2023-07-14 09:02:35 -0700

    Update views.py to connect db

--- app/xss_demo/views.py
+++ app/xss_demo/views.py
@@ -25,7 +25,9 @@ def _add_csp_header_hard(request):
         "img-src 'self';"
         "style-src 'self';"
         )
-
+def _create_db_connection(password="sdsdfsdfsdfdf"):
+    # connection code here
+    return
 
 def _add_csp_header(request):
     request.response.headers['Content-Security-Policy'] = (
